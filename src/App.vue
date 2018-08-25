f7565bd33514ff66de1e51c2c969123249f440c2
Gpingping <1206427971@qq.com>
2018-08-25 14:21:11 +0800

    后台管理系统提交

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- 路由出口，也就是所有的页面内容都是展示在这 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+  #app {
+    height: 100%;
+  }
+</style>
